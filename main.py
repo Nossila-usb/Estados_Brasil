2cbd19796b2284c52c8cf228107d6e5304e81bdd
Nossila-usb <minimonik1909@gmail.com>
2024-08-02 17:48:15 -0300

    versao 1.0

---
+++ main.py
@@ -0,0 +1,10 @@
+# lista 
+nomes = ['Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Distrito Federal','Espirito Santo','Goiás','Maranhão','Mato Grosso do Sul','Mato Grosso','Minas Gerais','Pará'
+         ,'Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins']
+
+# saida de dados 
+print(f'Quantidade de nomes: {len(nomes)}.\n')
+print('modo 1:\n')
+
+for nome in nomes:
+    print(nome)
